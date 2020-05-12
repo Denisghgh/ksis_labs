f26010a448d3383aac71728676b7b338610d0eb0
Denis March <denismarhenko123@gmail.com>
2020-05-12 14:57:35 +0300

    Added 2 lab

---
+++ 2LabLast/ClientProject/ClientProject/Form2.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ClientProject
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.clientNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clientLastNameTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(185, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(261, 24);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Enter your name (necessarily)";
+            // 
+            // connectButton
+            // 
+            this.connectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectButton.Location = new System.Drawing.Point(189, 224);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(264, 39);
+            this.connectButton.TabIndex = 11;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // clientNameTextBox
+            // 
+            this.clientNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clientNameTextBox.Location = new System.Drawing.Point(189, 107);
+            this.clientNameTextBox.Name = "clientNameTextBox";
+            this.clientNameTextBox.Size = new System.Drawing.Size(261, 20);
+            this.clientNameTextBox.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(185, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 24);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Enter your last name (optional)";
+            // 
+            // clientLastNameTextBox
+            // 
+            this.clientLastNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clientLastNameTextBox.Location = new System.Drawing.Point(189, 187);
+            this.clientLastNameTextBox.Name = "clientLastNameTextBox";
+            this.clientLastNameTextBox.Size = new System.Drawing.Size(264, 20);
+            this.clientLastNameTextBox.TabIndex = 13;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 352);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clientLastNameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.clientNameTextBox);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.TextBox clientNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox clientLastNameTextBox;
+    }
+}
---
+++ 2LabLast/ServerProject/ServerProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using CommonLibrary;
+
+namespace ServerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerClass server = new ServerClass(new BinaryMessagesSerializer());
+            server.Start();
+        }
+    }
+}
